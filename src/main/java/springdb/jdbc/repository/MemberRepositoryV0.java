c3b55321045b21e6ce459bd33fecea0286f0d210
Byeong-Uk Park <114344042+Rockernun@users.noreply.github.com>
2025-12-31 13:07:08 +0900

    feat(MemberRepositoryV0): 회원 아이디로 조회하는 기능 추가

--- src/main/java/springdb/jdbc/repository/MemberRepositoryV0.java
+++ src/main/java/springdb/jdbc/repository/MemberRepositoryV0.java
@@ -5,6 +5,7 @@ import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.sql.Statement;
+import java.util.NoSuchElementException;
 import lombok.extern.slf4j.Slf4j;
 import springdb.jdbc.connection.DbConnectionUtil;
 import springdb.jdbc.domain.Member;
@@ -36,8 +37,36 @@ public class MemberRepositoryV0 {
         }
     }
 
-    private void close(Connection con, Statement stmt, ResultSet rs) {
+    public Member findById(String memberId) throws SQLException {
+        String sql = "SELECT * FROM member WHERE member_id = ?";
 
+        Connection con = null;
+        PreparedStatement pstmt = null;
+        ResultSet rs = null;
+
+        try {
+            con = getConnection();
+            pstmt = con.prepareStatement(sql);
+            pstmt.setString(1, memberId);
+            rs = pstmt.executeQuery();
+
+            if (rs.next()) {
+                Member member = new Member();
+                member.setMemberId(rs.getString("member_id"));
+                member.setMoney(rs.getInt("money"));
+                return member;
+            } else {
+                throw new NoSuchElementException("Member not found memberId = " + memberId);
+            }
+        } catch (SQLException e) {
+            log.error("DB ERROR", e);
+            throw e;
+        } finally {
+            close(con, pstmt, rs);
+        }
+    }
+
+    private void close(Connection con, Statement stmt, ResultSet rs) {
         if (rs != null) {
             try {
                 rs.close();
