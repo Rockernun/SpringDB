7e6fb07a360d1f7c9f6427548fd08d48d78f5e0c
Byeong-Uk Park <114344042+Rockernun@users.noreply.github.com>
2026-01-05 14:47:36 +0900

    test(ConnectionTest): HikariDataSource 기반 커넥션 풀링 테스트 추가

--- src/test/java/springdb/jdbc/connection/ConnectionTest.java
+++ src/test/java/springdb/jdbc/connection/ConnectionTest.java
@@ -1,5 +1,6 @@
 package springdb.jdbc.connection;
 
+import com.zaxxer.hikari.HikariDataSource;
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.SQLException;
@@ -26,6 +27,20 @@ public class ConnectionTest {
         // DriverManagerDataSource - 항상 새로운 커넥션 획득
         DriverManagerDataSource dataSource = new DriverManagerDataSource(URL, USERNAME, PASSWORD);
         useDataSource(dataSource);
+    }
+
+    @Test
+    void dataSourceConnectionPool() throws SQLException, InterruptedException {
+        // 커넥션 풀링
+        HikariDataSource dataSource = new HikariDataSource();
+        dataSource.setJdbcUrl(URL);
+        dataSource.setUsername(USERNAME);
+        dataSource.setPassword(PASSWORD);
+        dataSource.setMaximumPoolSize(10);
+        dataSource.setPoolName("MyFirstConnectionPool");
+
+        useDataSource(dataSource);
+        Thread.sleep(1000);
     }
 
     private void useDataSource(DataSource dataSource) throws SQLException {
