5c5aa7fb3a7dcd5cf605c8ebb157b7fec0af1e1b
Byeong-Uk Park <114344042+Rockernun@users.noreply.github.com>
2026-01-12 16:34:02 +0900

    test(MemberServiceV2Test): 하나의 커넥션 내에서 정상적으로 동작하는지 테스트 추가

---
+++ src/test/java/springdb/jdbc/service/MemberServiceV2Test.java
@@ -0,0 +1,84 @@
+package springdb.jdbc.service;
+
+import static springdb.jdbc.connection.ConnectionConst.PASSWORD;
+import static springdb.jdbc.connection.ConnectionConst.URL;
+import static springdb.jdbc.connection.ConnectionConst.USERNAME;
+
+import java.sql.SQLException;
+import lombok.extern.slf4j.Slf4j;
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.springframework.jdbc.datasource.DriverManagerDataSource;
+import springdb.jdbc.domain.Member;
+import springdb.jdbc.repository.MemberRepositoryV2;
+
+/**
+ * 트랜잭션 - 커넥션 파라미터 전달 방식 동기화
+ */
+@Slf4j
+class MemberServiceV2Test {
+
+    public static final String MEMBER_A = "memberA";
+    public static final String MEMBER_B = "memberB";
+    public static final String MEMBER_EX = "ex";
+
+    private MemberRepositoryV2 memberRepository;
+    private MemberServiceV2 memberService;
+
+    @BeforeEach
+    void before() {
+        DriverManagerDataSource dataSource = new DriverManagerDataSource(URL, USERNAME, PASSWORD);
+        memberRepository = new MemberRepositoryV2(dataSource);
+        memberService = new MemberServiceV2(dataSource, memberRepository);
+    }
+
+    @AfterEach
+    void after() throws SQLException {
+        memberRepository.delete(MEMBER_A);
+        memberRepository.delete(MEMBER_B);
+        memberRepository.delete(MEMBER_EX);
+    }
+
+    @Test
+    @DisplayName("정상적으로 이체되었습니다.")
+    void accountTransfer() throws SQLException {
+        // given
+        Member memberA = new Member(MEMBER_A, 10000);
+        Member memberB = new Member(MEMBER_B, 10000);
+        memberRepository.save(memberA);
+        memberRepository.save(memberB);
+
+        // when
+        log.info("트랜잭션 시작");
+        memberService.accountTransfer(memberA.getMemberId(), memberB.getMemberId(), 2000);
+        log.info("트랜잭션 종료");
+
+        // then
+        Member findMemberA = memberRepository.findById(memberA.getMemberId());
+        Member findMemberB = memberRepository.findById(memberB.getMemberId());
+
+        Assertions.assertThat(findMemberA.getMoney()).isEqualTo(8000);
+        Assertions.assertThat(findMemberB.getMoney()).isEqualTo(12000);
+    }
+
+    @Test
+    @DisplayName("이체 중 예외가 발생하였습니다.")
+    void accountTransferEx() throws SQLException {
+        Member memberA = new Member(MEMBER_A, 10000);
+        Member memberEx = new Member(MEMBER_EX, 10000);
+        memberRepository.save(memberA);
+        memberRepository.save(memberEx);
+
+        Assertions.assertThatThrownBy(() -> memberService.accountTransfer(memberA.getMemberId(), memberEx.getMemberId(), 2000))
+                .isInstanceOf(IllegalStateException.class);
+
+        Member findMemberA = memberRepository.findById(memberA.getMemberId());
+        Member findMemberEx = memberRepository.findById(memberEx.getMemberId());
+
+        Assertions.assertThat(findMemberA.getMoney()).isEqualTo(10000);
+        Assertions.assertThat(findMemberEx.getMoney()).isEqualTo(10000);
+    }
+}
